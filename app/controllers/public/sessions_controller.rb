ef1b51339c9065216b80703bc5bee69daa2292c9
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-10-14 05:45:54 +0000

    [add]退会処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,23 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  private
+# アクティブであるかを判断するメソッド
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    customer = Customer.find_by(email: params[:customer][:email])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+
+    # 【処理内容4】 アクティブでない会員に対する処理
+    if customer.is_active
+      # アクティブならそのまま create アクションを実行させるためメソッド終了
+      return
+    else
+      # 非アクティブならサインアップ画面にリダイレクト
+      redirect_to new_customer_registration_path, alert: "このアカウントは退会済みです。新規登録してください。"
+    end
+  end
 end
