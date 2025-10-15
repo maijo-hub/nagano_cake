306e5944349f31cc157f6e0e1bb8ecea87ddd863
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-10-15 02:03:33 +0000

    [add]管理者商品登録モデル

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,43 @@
 class Admin::ItemsController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!  # 管理者認証（Devise使用前提）
+
   def index
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "商品を登録しました。"
+    else
+      render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item), notice: "商品情報を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image)
   end
 end
