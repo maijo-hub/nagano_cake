975c5fd2ba2abc49536c294323c9ec0df4fe3595
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-10-15 01:59:50 +0000

    [add]管理者商品登録コントローラー

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  # バリデーション
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # 商品画像（Active Storage）
+  has_one_attached :image
+
+  # 税込価格メソッド（必要なら）
+  def add_tax_price
+    (price * 1.1).round
+  end
 end
