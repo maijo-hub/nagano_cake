9e060ea1721745bcee777373dc1a59cd332d0a79
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-10-24 07:43:44 +0000

    [add]管理者側注文履歴一覧

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,8 +2,7 @@ class Admin::HomesController < ApplicationController
   # 管理者ログイン必須
   before_action :authenticate_admin!
 
-  # 管理者トップページ（注文履歴一覧など）
   def top
-    # 必要に応じて変数定義（例: @orders = Order.all）
+    @orders = Order.includes(:customer, :order_items).order(created_at: :desc).page(params[:page])
   end
 end
--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,8 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.includes(:item)
+  end
 end
