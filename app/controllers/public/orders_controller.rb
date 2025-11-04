db155f269f121a70530547ed215850457134542b
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-11-04 06:46:48 +0000

    [add]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,3 +1,26 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
+
+  def new
+    @order = Order.new
+  end
+
+  def check
+    @order = Order.new(order_params)
+
+    # 選択された住所をセット
+    if params[:order][:address_option] == "customer"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.full_name
+    end
+
+    @cart_items = current_customer.cart_items
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+        
 end
