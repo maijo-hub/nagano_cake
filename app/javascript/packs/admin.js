74d94467c1665393b6655cedcc3200d5c1ca8070
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-12-02 02:12:36 +0000

    [add]importをadmin.jsとpublic.jsに追加

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,1 +1,15 @@
-import "../stylesheets/admin/admin.scss";
+// app/javascript/packs/admin.js
+import "jquery";
+import "popper.js";
+import "bootstrap";                        // ← BootstrapのJS（必須）
+import "../stylesheets/admin/admin.scss";   // ← admin専用のSCSS（必須）
+
+// Rails helper は admin ページでも不要なら不要だが、必要なら入れてください
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+
+Rails && Rails.start && Rails.start();
+Turbolinks && Turbolinks.start && Turbolinks.start();
+ActiveStorage && ActiveStorage.start && ActiveStorage.start();
--- app/javascript/packs/public.js
+++ app/javascript/packs/public.js
@@ -1,1 +1,5 @@
+// app/javascript/packs/public.js
+import "jquery";
+import "popper.js";
+import "bootstrap";
 import "../stylesheets/public/public.scss";
